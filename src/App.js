a57957525273ce0299b33c0254dc3a19ce0d5370
jam9406 <teran2now@daum.net>
2019-04-02 14:05:17 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
 
+const customers =[{
+  'id': 1,
+  'image' : 'https://placeimg.com/64/64/any', 
+  'name': '홍길동',
+  'birthday' : '961222',
+  'gender' : '남자',
+  'job' : '대학생'
+}, 
+{
+ 'id': 2,
+'image' : 'https://placeimg.com/64/64/1', 
+'name': '이재민',
+'birthday' : '940614',
+'gender' : '남자',
+'job' : '취준생'
+},
+{
+'id': 3,
+'image' : 'https://placeimg.com/64/64/2', 
+'name': '이재혁',
+'birthday' : '971019',
+'gender' : '남자',
+'job' : '의경'
+}
+]
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-          <img src={logo} lat="logo" />
-          <h2> Let's develop management system!</h2>
-        </div>
+      <div>
+        {
+     customers.map(c=>{  //map을 통해 반복을 줄여줄 수 있음
+     return(
+      <Customer
+        key={c.id} // map 사용시 key 꼭 명시해줘야함
+        id={c.id}
+        image ={c.image}
+        name = {c.name}
+        birthday = {c.birthday}
+        gender = {c.gender}
+        job = {c.job}
+        />       
+     );
+  })
+}
+</div>
     );
-  }
+}
 }
 
 export default App;
